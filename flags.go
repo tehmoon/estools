0586d1bf0b93be1af7f199d6149fdf89fd64da47
Edouard Buschini <edouard.buschini@gmail.com>
2018-02-17 14:24:45 -0500

    Use tab instead of spaces

--- flags.go
+++ flags.go
@@ -1,42 +1,47 @@
 package main
 
 import (
-  "flag"
-  "os"
-  "fmt"
+	"flag"
+	"os"
+	"fmt"
 )
 
 type Flags struct {
-  QueryStringQuery string
-  StartDate string
-  Server string
-  Index string
-  Template string
+	QueryStringQuery string
+	StartDate string
+	Server string
+	Index string
+	Template string
 }
 
 func parseFlags() (*Flags) {
-  flags := &Flags{}
+	flags := &Flags{}
 
-  flag.StringVar(&flags.QueryStringQuery, "query", "*", "Elasticsearch query string query")
-  flag.StringVar(&flags.Server, "server", "http://localhost:9200", "Specify elasticsearch server to query")
-  flag.StringVar(&flags.Index, "index", "", "Specify the elasticsearch index to query")
-  flag.StringVar(&flags.Template, "template", "{{ . | json }}", "Specify Go text/template. You can use the function 'json' or 'json_indent'.")
+	flag.StringVar(&flags.QueryStringQuery, "query", "*", "Elasticsearch query string query")
+	flag.StringVar(&flags.Server, "server", "http://localhost:9200", "Specify elasticsearch server to query")
+	flag.StringVar(&flags.Index, "index", "", "Specify the elasticsearch index to query")
+	flag.StringVar(&flags.Template, "template", "{{ . | json }}", "Specify Go text/template. You can use the function 'json' or 'json_indent'.")
 
-  flag.Parse()
+	flag.Parse()
 
-  if flags.Index == "" {
-    fmt.Fprintln(os.Stderr, "-index is required")
-    flag.Usage()
-    os.Exit(2)
-  }
+	if flags.Index == "" {
+		fmt.Fprintln(os.Stderr, "-index is required")
+		flag.Usage()
+		os.Exit(2)
+	}
 
-  if flags.Template == "" {
-    fmt.Fprintln(os.Stderr, "-template cannot be empty")
-    flag.Usage()
-    os.Exit(2)
-  }
+	if flags.Template == "" {
+		fmt.Fprintln(os.Stderr, "-template cannot be empty")
+		flag.Usage()
+		os.Exit(2)
+	}
 
-  flags.Template = fmt.Sprintf("%s\n", flags.Template)
+	flags.Template = fmt.Sprintf("%s\n", flags.Template)
 
-  return flags
+	return flags
+}
+
+flag.Usage = func () {
+	fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
+	PrintDefaults()
 }
--- template.go
+++ template.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-  "text/template"
-  "encoding/json"
+	"text/template"
+	"encoding/json"
 )
 
 var (
-  functionTemplates = template.FuncMap{
-    "json": func(d interface{}) (string) {
-      payload, err := json.Marshal(d)
-      if err != nil {
-        return ""
-      }
+	functionTemplates = template.FuncMap{
+		"json": func(d interface{}) (string) {
+			payload, err := json.Marshal(d)
+			if err != nil {
+				return ""
+			}
 
-      return string(payload[:])
-    },
-    "json_indent": func(d interface{}) (string) {
-      payload, err := json.MarshalIndent(d, "", "  ")
-      if err != nil {
-        return ""
-      }
+			return string(payload[:])
+		},
+		"json_indent": func(d interface{}) (string) {
+			payload, err := json.MarshalIndent(d, "", "  ")
+			if err != nil {
+				return ""
+			}
 
-      return string(payload[:])
-    },
-  }
+			return string(payload[:])
+		},
+	}
 )
