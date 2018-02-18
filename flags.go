7714a0d585f4bdb3b22ea840e5cd4feab6eea2e1
Edouard Buschini <edouard.buschini@gmail.com>
2018-02-17 19:30:46 -0500

    Support for esfilters library
    You can now specify the -config and -filter-name option:
    
      - -config will get the config file generated by esfilters
      - -filter-name will get the filter from the config file and then use it for the query
      - -query will get support for the esfilters regexp engine only if -config is set
    
    Basically the documentation is at https://github.com/tehmoon/esfilters where you can
    learn how to use the CLI and how to build a query.

--- flags.go
+++ flags.go
@@ -12,12 +12,16 @@ type Flags struct {
 	Server string
 	Index string
 	Template string
+	ConfigFile string
+	FilterName string
 }
 
 func parseFlags() (*Flags) {
 	flags := &Flags{}
 
 	flag.StringVar(&flags.QueryStringQuery, "query", "*", "Elasticsearch query string query")
+	flag.StringVar(&flags.FilterName, "filter-name", "", "If specified use the esfilter's filter as the query")
+	flag.StringVar(&flags.ConfigFile, "config", "", "Use configuration file created by esfilters")
 	flag.StringVar(&flags.Server, "server", "http://localhost:9200", "Specify elasticsearch server to query")
 	flag.StringVar(&flags.Index, "index", "", "Specify the elasticsearch index to query")
 	flag.StringVar(&flags.Template, "template", "{{ . | json }}", "Specify Go text/template. You can use the function 'json' or 'json_indent'.")
@@ -36,12 +40,26 @@ func parseFlags() (*Flags) {
 		os.Exit(2)
 	}
 
+	if flags.FilterName != "" && flags.ConfigFile == "" {
+		fmt.Fprintln(os.Stderr, "when -filter-name is used, -config has to be specified")
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	if flags.FilterName != "" && (flags.QueryStringQuery != "*" && flags.QueryStringQuery != "") {
+		fmt.Fprintln(os.Stderr, "-filter-name and -query are mutually exclusive")
+		flag.Usage()
+		os.Exit(2)
+	}
+
 	flags.Template = fmt.Sprintf("%s\n", flags.Template)
 
 	return flags
 }
 
-flag.Usage = func () {
-	fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
-	PrintDefaults()
+func init() {
+	flag.Usage = func () {
+		fmt.Fprintf(os.Stderr, "Usage of %s: [-config=file] [-query=Query | <-config=file> <-filter-name=FilterName>] <-server=Url> <-index=Index> [-template=Template]\n", os.Args[0])
+		flag.PrintDefaults()
+	}
 }
