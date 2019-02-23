45dd4b0af5004d2b8b9f2d47c646081146c1cb55
Edouard Buschini <edouard.buschini@gmail.com>
2019-02-23 18:23:38 -0500

    fix typo

--- esalert/esalert/compile.sh
+++ esalert/esalert/compile.sh
@@ -5,7 +5,7 @@ rm build/*
 cd src
 
 go get ./...
-CC=$(which musl-gcc) go build -o ../build/esalertd --ldflags '-w -s -linkmode external -extldflags "-static"' . || true 
+CC=$(which musl-gcc) go build -o ../build/esalert --ldflags '-w -s -linkmode external -extldflags "-static"' . || true 
 
 cd ../build
 zip esalert-linux-x86_64.zip esalert
