7ecd1d48f2321c0115a9c613bd3cf87f27a8cb94
Edouard Buschini <edouard.buschini@gmail.com>
2019-02-24 13:09:51 -0500

    Show hashes when packaging is done

--- esalert/esalert/compile.sh
+++ esalert/esalert/compile.sh
@@ -10,3 +10,15 @@ CC=$(which musl-gcc) go build -o ../build/esalert --ldflags '-w -s -linkmode ext
 cd ../build
 zip esalert-linux-x86_64.zip esalert
 cd -
+
+cd
+for file in build/*.zip
+do
+	echo -n "${file} - "
+	./cryptocli -- \
+		file --path "${file}" --read -- \
+		dgst --algo sha256 -- \
+		hex --encode -- \
+		stdout 2> /dev/null
+	echo
+done
--- esalert/esalertd/compile.sh
+++ esalert/esalertd/compile.sh
@@ -10,3 +10,15 @@ CC=$(which musl-gcc) go build -o ../build/esalertd --ldflags '-w -s -linkmode ex
 cd ../build
 zip esalertd-linux-x86_64.zip esalertd
 cd -
+
+cd
+for file in build/*.zip
+do
+	echo -n "${file} - "
+	./cryptocli -- \
+		file --path "${file}" --read -- \
+		dgst --algo sha256 -- \
+		hex --encode -- \
+		stdout 2> /dev/null
+	echo
+done
